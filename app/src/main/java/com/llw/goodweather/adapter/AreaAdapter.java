f0aa3ec67483bb19315f145db8c658f5ae898ac3
llw <1270856521@qq.com>
2020-05-09 17:42:38 +0800

    2020-05-09 切换背景功能完成,增加下载APK的二维码图片

--- app/src/main/java/com/llw/goodweather/adapter/AreaAdapter.java
+++ app/src/main/java/com/llw/goodweather/adapter/AreaAdapter.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class AreaAdapter  extends BaseQuickAdapter<CityResponse.CityBean.AreaBean, BaseViewHolder> {
     public AreaAdapter(int layoutResId, @Nullable List<CityResponse.CityBean.AreaBean> data) {
         super(layoutResId, data);
+        
     }
 
     @Override

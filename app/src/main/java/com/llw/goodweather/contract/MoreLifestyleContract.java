40438609f149760f11a101e786e9731febef37c6
llw <1317057860@qq.com>
2020-08-08 16:12:36 +0800

    2020-08-08 GoodWeather1.9版本开发完成，新增更多天气预报、空气质量、生活指数等数据并且优化了UI展示效果。

---
+++ app/src/main/java/com/llw/goodweather/contract/MoreLifestyleContract.java
@@ -0,0 +1,54 @@
+package com.llw.goodweather.contract;
+
+import com.llw.goodweather.api.ApiService;
+import com.llw.goodweather.bean.DailyResponse;
+import com.llw.goodweather.bean.LifestyleResponse;
+import com.llw.mvplibrary.base.BasePresenter;
+import com.llw.mvplibrary.base.BaseView;
+import com.llw.mvplibrary.net.NetCallBack;
+import com.llw.mvplibrary.net.ServiceGenerator;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+/**
+ * 更多生活指数订阅器
+ */
+public class MoreLifestyleContract {
+
+    public static class MoreLifestylePresenter extends BasePresenter<IMoreLifestyleView> {
+
+        /**
+         * 更多生活指数  V7
+         * @param location  城市id
+         */
+        public void worldCity(String location) {
+            ApiService service = ServiceGenerator.createService(ApiService.class,3);
+            service.lifestyle("0",location).enqueue(new NetCallBack<LifestyleResponse>() {
+                @Override
+                public void onSuccess(Call<LifestyleResponse> call, Response<LifestyleResponse> response) {
+                    if(getView() != null){
+                        getView().getMoreLifestyleResult(response);
+                    }
+                }
+
+                @Override
+                public void onFailed() {
+                    if(getView() != null){
+                        getView().getDataFailed();
+                    }
+                }
+            });
+        }
+
+    }
+
+    public interface IMoreLifestyleView extends BaseView {
+
+        //更多生活指数返回数据 V7
+        void getMoreLifestyleResult(Response<LifestyleResponse> response);
+
+        //错误返回
+        void getDataFailed();
+    }
+}
--- app/src/main/java/com/llw/goodweather/ui/MoreLifestyleActivity.java
+++ app/src/main/java/com/llw/goodweather/ui/MoreLifestyleActivity.java
@@ -1,19 +1,92 @@
 package com.llw.goodweather.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.llw.goodweather.R;
+import com.llw.goodweather.adapter.MoreLifestyleAdapter;
+import com.llw.goodweather.bean.LifestyleResponse;
+import com.llw.goodweather.contract.MoreLifestyleContract;
+import com.llw.goodweather.utils.CodeToStringUtils;
+import com.llw.goodweather.utils.Constant;
+import com.llw.goodweather.utils.StatusBarUtil;
+import com.llw.goodweather.utils.ToastUtils;
+import com.llw.mvplibrary.mvp.MvpActivity;
+import com.llw.mvplibrary.utils.RecyclerViewAnimation;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Response;
+
+import static com.llw.mvplibrary.utils.RecyclerViewAnimation.runLayoutAnimation;
 
 /**
  * 更多生活指数信息
  */
-public class MoreLifestyleActivity extends AppCompatActivity {
+public class MoreLifestyleActivity extends MvpActivity<MoreLifestyleContract.MoreLifestylePresenter> implements MoreLifestyleContract.IMoreLifestyleView {
+
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.rv)
+    RecyclerView rv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_more_lifestyle);
+    public void initData(Bundle savedInstanceState) {
+        StatusBarUtil.transparencyBar(context);//透明状态栏
+        Back(toolbar);
+        showLoadingDialog();
+        tvTitle.setText(getIntent().getStringExtra("cityName"));
+        mPresent.worldCity(getIntent().getStringExtra("locationId"));
     }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_more_lifestyle;
+    }
+
+    @Override
+    protected MoreLifestyleContract.MoreLifestylePresenter createPresent() {
+        return new MoreLifestyleContract.MoreLifestylePresenter();
+    }
+
+    /**
+     * 更多生活质量数据返回
+     *
+     * @param response
+     */
+    @Override
+    public void getMoreLifestyleResult(Response<LifestyleResponse> response) {
+        dismissLoadingDialog();
+        if (response.body().getCode().equals(Constant.SUCCESS_CODE)) {
+            List<LifestyleResponse.DailyBean> data = response.body().getDaily();
+            if (data != null && data.size() > 0) {
+                MoreLifestyleAdapter adapter = new MoreLifestyleAdapter(R.layout.item_more_lifestyle_list, data);
+                rv.setLayoutManager(new LinearLayoutManager(context));
+                rv.setAdapter(adapter);
+                runLayoutAnimation(rv);
+            } else {
+                ToastUtils.showShortToast(context, "生活质量数据为空");
+            }
+        } else {
+            ToastUtils.showShortToast(context, CodeToStringUtils.WeatherCode(response.body().getCode()));
+        }
+    }
+
+    /**
+     * 其他异常返回
+     */
+    @Override
+    public void getDataFailed() {
+        dismissLoadingDialog();
+        ToastUtils.showShortToast(context, "更多天气数据获取异常");
+    }
+
 }
